daf2ae4538831b73f54b055cc647221eec15235e
AVIRAL1854 <aviralrawat365@gmail.com>
2024-10-09 21:51:37 +0530

    donateToCampaign api implemented

--- TRY/my-app/app/apis/donateToCampaign/route.tsx
+++ TRY/my-app/app/apis/donateToCampaign/route.tsx
@@ -1,42 +1,49 @@
 import Web3 from "web3";
 import { NextResponse, NextRequest } from "next/server";
-import {abi} from "@/Components/abi";
-
+import { abi } from "@/Components/abi";
 
 const infuraUrl = "http://127.0.0.1:8545/";
-const contractAddress = process.env.fake_contract_address;   
+const contractAddress = process.env.fake_contract_address;
 
+export async function POST(req: NextRequest) {
+  const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl));
+  const myContract = new web3.eth.Contract(abi, contractAddress);
+  const body = await req.json();
+  let ans;
 
-export async function POST(req:NextRequest){
+  const donateCampaign = async (id) => {
+    try {
+      const ResponseDonateCampaign = await myContract.methods
+        .donateToCampaign(id)
+        .send({
+          from: body.data.accountAddress,
+          gas: "3000000",
+          value: body.data.value,
+        });
 
+      console.log("this is the responseData : " + ResponseDonateCampaign);
 
-    const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl));
-    const myContract = new web3.eth.Contract(abi, contractAddress);
-    const body= await req.json();
-    let ans;
-
-    const donateCampaign=async(id)=>{
-        try{
-
-            const ResponseDonateCampaign= await myContract.methods.donateToCampaign(id).send({from:body.data.accountAddress, gas:"3000000", value:body.data.value});
-
-            console.log("this is the responseData : "+ResponseDonateCampaign);
-            ans=ResponseDonateCampaign;
-
-
-
-        }
-        catch(error){
-            console.log("this is the custom error:"+ error.message);
-        }
+      // Convert BigInt values to strings
+      const serializeCampaigns = (ResponseDonateCampaign) => {
+        return ResponseDonateCampaign.map((ResponseDonateCampaign) => {
+          return Object.fromEntries(
+            Object.entries(ResponseDonateCampaign).map(([key, value]) => [
+              key,
+              typeof value === "bigint" ? value.toString() : value,
+            ])
+          );
+        });
+      };
+      ans = serializeCampaigns;
+    } catch (error) {
+      console.log("this is the custom error:" + error.message);
     }
-    
+  };
 
-    await donateCampaign(body.data.id);
+  await donateCampaign(body.data.id);
 
-    return NextResponse.json({
-        "message":"this was successfull and done",
-        "response":ans
-    })
-
+  return NextResponse.json({
+    message: "this was successfull and done",
+    response: ans,
+  });
 }
