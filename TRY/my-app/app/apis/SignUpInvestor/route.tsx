cf4b12de989aef7561b687ad6aaf7686138144c6
AVIRAL1854 <aviralrawat365@gmail.com>
2024-11-18 20:02:14 +0530

    login api

---
+++ TRY/my-app/app/apis/SignUpInvestor/route.tsx
@@ -0,0 +1,28 @@
+import { NextRequest,NextResponse } from "next/server";
+
+export async function POST(req:NextRequest){
+
+const body=await req.json();
+const walletAddress=body.data.walletAddress;
+const email=body.data.email;
+const password=body.data.password;
+
+
+let ans;
+try{
+
+    console.log("this is the :"+walletAddress+"\nthis is email"+email+"\nthis is the password"+password);
+
+    // check whether already present or not 
+
+    // if not then create a new entry in database
+
+}catch(error){
+    console.log("this is the custom error :"+error.message);
+}
+
+return NextResponse.json({
+    "message":"this is working fine",
+    "response":ans
+})
+}
